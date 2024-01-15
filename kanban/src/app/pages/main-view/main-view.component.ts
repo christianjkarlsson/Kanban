d9fcefbdd2bb1a7a5465bcb44be60fb05ce3bda4
Christian Kröning <karlsson.j.christian@gmail.com>
2024-01-15 15:19:17 +0100

    Support drag'n'drop kanban

--- kanban/src/app/pages/main-view/main-view.component.ts
+++ kanban/src/app/pages/main-view/main-view.component.ts
@@ -1,12 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+import {
+  NgFor,
+
+} from '@angular/common';
+import { Board } from '../../models/board.model';
+import { Column } from '../../models/column.model';
 
 @Component({
   selector: 'app-main-view',
   standalone: true,
-  imports: [],
+  imports: [DragDropModule, NgFor],
   templateUrl: './main-view.component.html',
-  styleUrl: './main-view.component.scss'
+  styleUrl: './main-view.component.scss',
 })
-export class MainViewComponent {
 
+export class MainViewComponent implements OnInit {
+
+  constructor() { }
+
+  board: Board = new Board('Test Board', [
+    new Column('Todo', [
+      'Get to work',
+      'Pick up groceries',
+      'Go home',
+      'Fall asleep'
+    ]),
+    new Column('Implementing', [
+      "Lorem ipsum",
+      "foo",
+      "This was in the 'Research' column"
+    ]),
+    new Column('Done', [
+      'Get up',
+      'Brush teeth',
+      'Take a shower',
+      'Check e-mail',
+      'Walk dog'
+    ])
+  ]);
+
+  ngOnInit() {
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
 }
+
